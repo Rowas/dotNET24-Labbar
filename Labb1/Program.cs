97ba9a6e98885cb04be7fac1f6574e3a4cf4958b
Rowas <white_wolf_the_white@hotmail.com>
2024-09-06 14:04:12 +0200

    Finished the StringSanityCheck function. Checks that no letters are in the found numbers between two indexes.
    Created and finished StringTrashRemover function.
    Cleans up the string and makes it into a double for use in calculating the sum of all the fund number-strings.
    
    Cleaned up the main function code.
    
    Commented out all sanity check writelines so that output is in-line with the assignment.

--- Labb1/Program.cs
+++ Labb1/Program.cs
@@ -36,6 +36,30 @@ static bool StringSanityCheck(string sanityCheck)
     return sane;
 }
 
+static double StringTrashRemover(string stringWithTrash)
+{
+    double trashFreeDouble = 0;
+    char[] charWithoutTrash = new char[stringWithTrash.Length];
+    char c = ' ';
+    int y = 0;
+    for (int i = 0; i <= stringWithTrash.Length - 1; i++)
+    {
+        c = char.Parse(stringWithTrash.Substring(i, 1));
+
+        if (char.IsDigit(c) == true)
+        {
+            charWithoutTrash[y] = c;
+            y++;
+        }
+    }
+
+    string stringWithoutTrash = new string(charWithoutTrash);
+
+    trashFreeDouble = double.Parse(stringWithoutTrash);
+
+    return trashFreeDouble;
+}
+
 static (string, string) BeforeAfterNumbers(string userProvidedString, int index1, int index2)
 {
     string remainingNumbersBefore = userProvidedString.Remove(index1);
@@ -46,46 +70,70 @@ static (string, string) BeforeAfterNumbers(string userProvidedString, int index1
 string userProvidedString = "";
 int index1 = 0;
 int index2 = 1;
+double[] addFoundNumbers = new double[1500];
+string stringToColour = "";
+bool sane = true;
+int newIndex2 = 0;
+int addIndex = 0;
 
+Console.ForegroundColor = ConsoleColor.Gray;
 Console.WriteLine("Please provide a string to evaluate: ");
 userProvidedString = Console.ReadLine();
 
-int newIndex2 = IndexSearch(userProvidedString, index1, index2);
-
-Console.WriteLine($"{index1},{newIndex2}");
-
-string stringToColour = "";
-
-stringToColour = FindNumbers(userProvidedString, index1, newIndex2);
-
-Console.WriteLine(stringToColour);
-
-bool sane = StringSanityCheck(stringToColour);
-
-if (sane == true)
+while (true)
 {
-    Console.WriteLine("String is safe to use.");
-}
-else
-{
-    Console.WriteLine("String is not safe, trash and move on.");
-    return;
-}
+    if (index2 == userProvidedString.Length - 1)
+    {
+        break;
+    }
+    newIndex2 = IndexSearch(userProvidedString, index1, index2);
 
-(string numbersAfter, string numbersBefore) = BeforeAfterNumbers(userProvidedString, index1, newIndex2);
+    // Console.WriteLine($"{index1},{newIndex2}"); --Sanity check of IndexSearch, leaving for future troubleshooting. 
 
+    stringToColour = FindNumbers(userProvidedString, index1, newIndex2);
+
+    //Console.WriteLine(stringToColour); --Sanity check of result from FindNumbers(), leaving for future troubleshooting.
+
+    sane = StringSanityCheck(stringToColour);
+
+
+    if (newIndex2 < 0)
+    {
+        //Console.WriteLine("String is not safe, trash and move on."); --Confirmation of result from StringSanityCheck, leaving for future troubleshooting.
+        index1++;
+        index2++;
+        continue;
+    }
+    else if (sane == false)
+    {
+        //Console.WriteLine("String is not safe, trash and move on."); --Confirmation of result from StringSanityCheck, leaving for future troubleshooting.
+        index1++;
+        index2++;
+        continue;
+    }
+    else
+    {
+        //Console.WriteLine("String is safe to use."); --Confirmation of result from StringSanityCheck, leaving for future troubleshooting.
+        addFoundNumbers[addIndex] = StringTrashRemover(stringToColour);
+        addIndex++;
+    }
+
+    (string numbersAfter, string numbersBefore) = BeforeAfterNumbers(userProvidedString, index1, newIndex2);
+
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Gray;
+    Console.Write(numbersBefore);
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write(stringToColour);
+    Console.ForegroundColor = ConsoleColor.Gray;
+    Console.Write(numbersAfter);
+    index1++;
+    index2++;
+}
 Console.WriteLine();
-Console.ForegroundColor = ConsoleColor.Gray;
-Console.Write(numbersBefore);
-Console.ForegroundColor = ConsoleColor.Red;
-Console.Write(stringToColour);
-Console.ForegroundColor = ConsoleColor.Gray;
-Console.WriteLine(numbersAfter);
-
-double[] addFoundNumbers = new double[1500];
-addFoundNumbers[index1] = Int32.Parse(stringToColour);
-addFoundNumbers[1] = 1000000;
-addFoundNumbers[2] = 1000000;
+Console.WriteLine();
 double sumFoundNumbers = addFoundNumbers.Sum();
+Console.WriteLine("The sum of all the found numbers is: ");
 Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine(sumFoundNumbers);
+Console.Write(sumFoundNumbers);
+Console.ForegroundColor = ConsoleColor.Gray;
