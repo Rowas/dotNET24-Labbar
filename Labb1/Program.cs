7ec0bd675bc4a655c45e9f088d00660247c5988e
Rowas <white_wolf_the_white@hotmail.com>
2024-09-06 10:28:42 +0200

    FindNumbers function completed, provides string of numbers between previous found indexes.
    StringSanityCheck function completed, provides string sanity, if letter found in string, returns false, new string should be found.

--- Labb1/Program.cs
+++ Labb1/Program.cs
@@ -2,18 +2,39 @@
 
 static int IndexSearch(string searchString, int charSearch, int beginIndex)
 {
+    int foundIndex;
     char searchChar = searchString[charSearch];
-    int foundIndex = searchString.IndexOf(searchChar,beginIndex);
-
+    foundIndex = searchString.IndexOf(searchChar, beginIndex);
     return foundIndex;
 }
-/*
-static double FindNumbers(int startIndex, int endIndex)
+
+static string FindNumbers(string searchString, int startIndex, int endIndex)
 {
-    return numbersToAdd;
+    char[] stringChars = new char[searchString.Length];
+
+    for (int i = startIndex; i <= endIndex; i++)
+    {
+        stringChars[i] = Char.Parse(searchString.Substring(i, 1));
+    }
+    string foundNumString = new string(stringChars);
+
     return foundNumString;
 }
 
+static bool StringSanityCheck(string sanityCheck)
+{
+    bool sane = true;
+    foreach (char c in sanityCheck)
+    {
+        if (char.IsLetter(c) == true)
+        {
+            sane = false;
+            break;
+        }
+    }
+    return sane;
+}
+/*
 static string BeforeAfterNumbers(string numbersFound)
 {
 
@@ -33,13 +54,30 @@ static double[] foundNumbersAddition(double[] addFoundNumbers)
 
 */
 
-string userPorivdedString = "";
+string userProvidedString = "";
 int index1 = 0;
 int index2 = 1;
 
 Console.WriteLine("Please provide a string to evaluate: ");
-userPorivdedString = Console.ReadLine();
+userProvidedString = Console.ReadLine();
 
-index2 = IndexSearch(userPorivdedString, index1, index2);
+int newIndex2 = IndexSearch(userProvidedString, index1, index2);
 
-Console.Write($"{index1},{index2}");
+Console.WriteLine($"{index1},{newIndex2}");
+
+string stringToColour = "";
+
+stringToColour = FindNumbers(userProvidedString, index1, newIndex2);
+
+Console.WriteLine(stringToColour);
+
+bool sane = StringSanityCheck(stringToColour);
+
+if (sane == true)
+{
+    Console.WriteLine("String is safe to use.");
+}
+else
+{
+    Console.WriteLine("String is not safe, trash and move on.");
+}
